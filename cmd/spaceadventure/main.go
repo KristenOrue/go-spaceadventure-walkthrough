d9e969c685873d018c8764c20feb03b0b3d3e9d1
Kristen Orue <korue113@gmail.com>
2019-11-09 14:17:45 -0800

    Restructured files

---
+++ cmd/spaceadventure/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "github.com/KristenOrue/spaceadventure_solution/internal/spaceadventure"
+
+func main() {
+	var ps = spaceadventure.PlanetarySystem{"Solare System"}
+	spaceadventure.Start(ps)
+}
+
+
